2a25af083a7858222872d29affef9945ccececcd
vrnico <nico.daunt@gmail.com>
2018-06-12 14:05:45 -0700

    add readme and sfx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 === 0){
+      this.evenNumbers.push(firedNumber);
+      this.playBark();
+
+    }  else {
+      this.oddNumbers.push(firedNumber);
+      this.playFart();
+
+    }
+   }
+   playFart(){
+    let audio = new Audio();
+    audio.src = "../assets/sounds/fart.mp3";
+    audio.load();
+    audio.play();
+}
+
+    playBark(){
+     let audio = new Audio();
+     audio.src = "../assets/sounds/bark.mp3";
+     audio.load();
+     audio.play();
+    }
+
+
   title = 'app';
 }
